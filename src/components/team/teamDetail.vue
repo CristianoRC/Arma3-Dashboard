41a8ca2fffd4c1b41bdf62e2c60d94343791adb2
Cristiano <contato@cristianoprogramador.com>
2020-06-07 12:53:48 -0300

    Cria o card de detalhes de equipe

---
+++ src/components/team/teamDetail.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card class="mx-auto" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">Equipe</div>
+        <v-list-item-title class="headline mb-1">{{team.name}}</v-list-item-title>
+        <v-list-item-subtitle>{{names}}</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="100" color="primary">
+        <p class="display-1 white--text">{{temAvatar}}</p>
+      </v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn text>Detalhes</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  computed: {
+    names() {
+      if (this.team.persons.length > 1)
+        return this.team.persons.reduce((acc, cur) => {
+          return `${acc.militaryRank} ${acc.name}, ${cur.militaryRank} ${cur.name}`;
+        });
+
+      return `${this.team.persons[0].militaryRank} ${this.team.persons[0].name}`;
+    },
+    temAvatar() {
+      return this.team.name.charAt(0).toUpperCase();
+    }
+  },
+  props: ["team"]
+};
+</script>
+
+<style>
+</style>
--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -1,13 +1,69 @@
 <template>
-  <p></p>
+  <v-container fluid>
+    <v-row justify="center" class="mt-5">
+      <v-col sm="10">
+        <v-card>
+          <v-card-title class="text-center justify-center py-6">
+            <v-row>
+              <v-col sm="12">
+                <img src="../assets/Logo_CinzaVerde.png" alt="Logo 14º BFN" height="100px" />
+              </v-col>
+              <v-col class="mt-5" sm="12">
+                <h1 class="font-weight-bold display-2">Equipes em campo</h1>
+              </v-col>
+              <v-col sm="12">
+                <v-btn small @click="addNewTeam" color="primary">
+                  Adicionar nova equipe
+                  <v-icon right dark>mdi-account-multiple-plus</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card-title>
+
+          <v-card-text>
+            <v-row>
+              <v-col sm="12" md="4" v-for="(team, index) in teams" :key="index">
+                <team-detail :team="team"></team-detail>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import TeamDetail from "../components/team/teamDetail";
 export default {
-
-}
+  data: () => ({
+    teams: [
+      {
+        name: "Alfa",
+        persons: [{ name: "Cunha", militaryRank: "Sd", function: "Mg" }]
+      },
+      {
+        name: "Bravo",
+        persons: [
+          { name: "Cristiano", militaryRank: "Sd", function: "Mg" },
+          { name: "Cunha", militaryRank: "Sd", function: "Mg" }
+        ]
+      }
+    ]
+  }),
+  methods: {
+    saveOnLocalStorage() {
+      localStorage.setItem("teams", JSON.stringify(this.teams));
+    },
+    getFromLocalStorage() {
+      const teams = JSON.parse(localStorage.getItem("teams"));
+      if (teams !== null) this.teams = teams;
+    },
+    addNewTeam() {}
+  },
+  components: { TeamDetail }
+};
 </script>
 
 <style>
-
 </style>
